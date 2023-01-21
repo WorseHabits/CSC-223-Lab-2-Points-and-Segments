aad143a699468faaa3f10c5959184eeff02e8762
joshberger5 <joshberger5@yahoo.com>
2023-01-21 12:45:02 -0500

    SegmentNodeDatabase start
    I started SegmentNodeDatabase, but didn't get too far.  I had to stop working so I just wanted to upload what I had.

--- src/input/components/segment/SegmentNodeDatabase.java
+++ src/input/components/segment/SegmentNodeDatabase.java
@@ -1,5 +1,52 @@
 package input.components.segment;
 
-public class SegmentNodeDatabase {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
+import input.components.point.PointNode;
+
+public class SegmentNodeDatabase {
+	private Map<PointNode, Set<PointNode>> _adjLists;
+	
+	public SegmentNodeDatabase() {
+		_adjLists = new HashMap<PointNode, Set<PointNode>>();
+	}
+	
+	public SegmentNodeDatabase(Map<PointNode, Set<PointNode>> adjLists) {
+		_adjLists = new HashMap<PointNode, Set<PointNode>>(adjLists);
+	}
+	
+	public int numUndirectedEdges() {
+		int total = 0;
+		for (Set<PointNode> adjacencies : _adjLists.values()) {
+			total = total + adjacencies.size();
+		}
+		return total / 2;
+	}
+	
+	public void addDirectedEdge(PointNode a, PointNode b) {
+		
+	}
+	
+	public void addUndirectedEdge(PointNode a, PointNode b) {
+		if (!_adjLists.containsKey(a) && !_adjLists.containsKey(b)) {
+			
+		}
+	}
+	
+	public void addAjacencyList(PointNode point, List<PointNode> adjList) {
+		
+	}
+	
+	public List<SegmentNode> asSegmentList() {
+		return null;
+	}
+	
+	public List<SegmentNode> asUniqueSegmentList() {
+		return null;
+	}
 }
