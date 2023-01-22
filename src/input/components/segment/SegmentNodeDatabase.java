9a60d77aa2900d23d01b4d692e87e78eca4e77b4
joshberger5 <joshberger5@yahoo.com>
2023-01-22 16:33:23 -0500

    changed addAdjacencyList

--- src/input/components/segment/SegmentNodeDatabase.java
+++ src/input/components/segment/SegmentNodeDatabase.java
@@ -72,12 +72,33 @@ public class SegmentNodeDatabase {
 		}
 	}
 	
-	// converts the passed-in list to a set and adds it to the map
-	// idk what you're supposed to do if the point is already in the map
 	public void addAjacencyList(PointNode point, List<PointNode> adjList) {
-		Set<PointNode> adjSet = new HashSet<PointNode>();
-		adjSet.addAll(adjList);
-		_adjLists.put(point, adjSet);
+		// if the map contains point
+		// check to see if point already has each of the adjacencies in the passed-in list
+		// if not, add them
+		if (_adjLists.containsKey(point)) {
+			for (PointNode adjacency : adjList) {
+				if (!_adjLists.get(point).contains(adjacency)) {
+					Set<PointNode> pointADJ = new HashSet<PointNode>(_adjLists.get(point));
+					pointADJ.add(adjacency);
+					_adjLists.put(point, pointADJ);
+				}
+			}
+		}
+		// if the map doesn't contain point
+		// add it and the passed-in adjacency list as a set
+		else if (!_adjLists.containsKey(point)) {
+			Set<PointNode> adjSet = new HashSet<PointNode>();
+			adjSet.addAll(adjList);
+			_adjLists.put(point, adjSet);
+		}
+		// check to see if the points in the adjacency list are in the map
+		// if not, add them and point as the only point in their adjacency list
+		for (PointNode adjacency : adjList) {
+			if (!_adjLists.containsKey(adjacency)) {
+				_adjLists.put(adjacency, new HashSet<PointNode>(Arrays.asList(point)));
+			}
+		}
 	}
 	
 	// creates a list
